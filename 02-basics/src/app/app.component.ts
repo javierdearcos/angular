199ba5bc7f0bf6c015ef9614291fb920a2a930ac
Javier de Arcos <fj.dearcos@gmail.com>
2023-08-17 15:45:09 +0200

    Refactor to extract the counter to a component

--- 02-basics/src/app/app.component.ts
+++ 02-basics/src/app/app.component.ts
@@ -5,24 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  private DEFAULT_VALUE = 10;
-
-  public title: string = 'Counter App';
-  public counter: number = this.DEFAULT_VALUE;
-
-  resetCounter():void {
-    this.counter = this.DEFAULT_VALUE;
-  }
-
-  increaseCounter(): void {
-    this.addCounter(1);
-  }
-
-  decreaseCounter(): void {
-    this.addCounter(-1);
-  }
-
-  private addCounter(value: number): void {
-    this.counter += value;
-  }
+  public title: string = 'My first App';
 }
