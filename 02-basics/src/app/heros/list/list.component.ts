ea17c12e3b06da92765fc88869a0f12e52f5d832
Javier de Arcos <fj.dearcos@gmail.com>
2023-08-17 19:12:46 +0200

    Use ng-template and ng-if-else to show the last hero name removed

--- 02-basics/src/app/heros/list/list.component.ts
+++ 02-basics/src/app/heros/list/list.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heros-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent {
-  public heroNames: string[] = ['Spiderman', 'Iron Man', 'Hulk', 'Thor'];
+  public heroNames: string[] = [...ListComponentConstants.DEFAULT_HEROS];
+  public lastElementRemoved?: string;
+
+  removeLastHero(): void {
+    if (this.heroNames.length > 0) {
+      this.lastElementRemoved = this.heroNames.pop();
+    }
+  }
+
+  resetHeros(): void {
+    this.heroNames = [...ListComponentConstants.DEFAULT_HEROS];
+    delete this.lastElementRemoved;
+  }
+
+  ListComponentConstants = ListComponentConstants;
+}
+
+class ListComponentConstants {
+ public static readonly DEFAULT_HEROS: string[] = ['Spiderman', 'Iron Man', 'Hulk', 'Thor'];
 }
