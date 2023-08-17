d6c984b56fe87ee7e24f3e21e811a49ba03c1452
Javier de Arcos <fj.dearcos@gmail.com>
2023-08-17 18:34:16 +0200

    Add usage of directive *ngIf to show change buttons

--- 02-basics/src/app/heros/hero/hero.component.ts
+++ 02-basics/src/app/heros/hero/hero.component.ts
@@ -3,12 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heros-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css']
+  styleUrls: ['./hero.component.css'],
 })
 export class HeroComponent {
-  public name: string = 'Iron Man';
-  public fullName: string = 'Tony Stark';
-  public age: number = 45;
+
+  public name: string = HeroComponentConstants.DEFAULT_NAME;
+  public fullName: string = HeroComponentConstants.DEFAULT_FULL_NAME;
+  public age: number = HeroComponentConstants.DEFAULT_AGE;
 
   get capitalizedName(): string {
     return this.name.toUpperCase();
@@ -26,4 +27,18 @@ export class HeroComponent {
   changeAge(): void {
     this.age = 16;
   }
+
+  resetForm(): void {
+    this.name = HeroComponentConstants.DEFAULT_NAME;
+    this.fullName = HeroComponentConstants.DEFAULT_FULL_NAME;
+    this.age = HeroComponentConstants.DEFAULT_AGE;
+  }
+
+  public HeroComponentConstants = HeroComponentConstants;
+}
+
+class HeroComponentConstants {
+  public static readonly DEFAULT_NAME: string = 'Iron Man';
+  public static readonly DEFAULT_FULL_NAME: string = 'Tony Stark';
+  public static readonly DEFAULT_AGE: number = 45;
 }
