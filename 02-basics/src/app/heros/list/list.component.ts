823a6e70e75e6a52e00c73c61774cf075f58891c
Javier de Arcos <fj.dearcos@gmail.com>
2023-08-17 18:39:27 +0200

    Add hero list using ngFor directive

--- 02-basics/src/app/heros/list/list.component.ts
+++ 02-basics/src/app/heros/list/list.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
-
+  public heroNames: string[] = ['Spiderman', 'Iron Man', 'Hulk', 'Thor'];
 }
