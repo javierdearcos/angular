a82ee00d40a30e65e65e440ed718b024aae3e20d
Javier de Arcos <fj.dearcos@gmail.com>
2023-08-26 18:13:59 +0200

    Show gif image

--- 04-gifs-app/src/app/gifs/pages/home/home-page.component.ts
+++ 04-gifs-app/src/app/gifs/pages/home/home-page.component.ts
@@ -5,10 +5,9 @@ import { Gif } from '../../interfaces/gifs.interfaces';
 @Component({
   selector: 'gifs-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent {
-
   constructor(private gifsService: GifsService) {}
 
   get gifs(): Gif[] {
