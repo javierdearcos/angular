75e9cf3d64ff4d65b88ba9fabf7e3078336526f9
Javier de Arcos <fj.dearcos@gmail.com>
2023-09-02 21:42:52 +0200

    Add sidebar component

--- 05-countryApp/src/app/shared/shared.module.ts
+++ 05-countryApp/src/app/shared/shared.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomePageComponent } from './pages/home-page/home-page.component';
+
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 
 @NgModule({
   declarations: [
     AboutPageComponent,
-    HomePageComponent
+    HomePageComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     AboutPageComponent,
-    HomePageComponent
+    HomePageComponent,
+    SidebarComponent
   ]
 })
 export class SharedModule { }
