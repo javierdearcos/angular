3ab6d981297e67207602aed53b48f8dfe968383d
Javier de Arcos <fj.dearcos@gmail.com>
2023-09-23 16:44:55 +0200

    Trigger search by capital using the Countries Service and show results in By Capital Page

--- 05-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ 05-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'countries-by-capital-page',
   templateUrl: './by-capital-page.component.html',
-  styles: [
-
-  ]
+  styles: [],
 })
 export class ByCapitalPageComponent {
+  constructor(private countriesService: CountriesService) {}
 
+  public countries: Country[] = [];
+
+  searchByCapital(capital: string): void {
+    this.countriesService
+      .searchCountryByCapital(capital)
+      .subscribe((countries) => (this.countries = countries));
+  }
 }
