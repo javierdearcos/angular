91f3aef32275af23a9ee956d5d43ff1c6533fc8b
Javier de Arcos <fj.dearcos@gmail.com>
2023-09-23 16:33:26 +0200

    Create Countries service using RestCountries API

---
+++ 05-countryApp/src/app/countries/services/countries.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Country } from '../interfaces/country';
+
+@Injectable({providedIn: 'root'})
+export class CountriesService {
+  constructor(private httpClient: HttpClient) { }
+
+  private restCountriesApiUrl = 'https://restcountries.com/v3.1';
+
+  searchCountryByCapital(capital: string): Observable<Country[]> {
+    return this.httpClient.get<Country[]>(`${this.restCountriesApiUrl}/capital/${capital}`);
+  }
+}
